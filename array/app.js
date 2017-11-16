7517990e0b606a2180b162ca8ff19a286af3a366
christelletalbot <christelletalbot12@gmail.com>
2017-11-16 15:24:18 +0100

    Exercie 3

--- array/app.js
+++ array/app.js
@@ -2,4 +2,7 @@ var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 var array = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
 
 console.log(array)
+
 console.log(array [2])
+
+console.log(array [4])
