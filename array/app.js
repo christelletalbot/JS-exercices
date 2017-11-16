691336aedd7b103021eb777a2b66240947fdd799
christelletalbot <christelletalbot12@gmail.com>
2017-11-16 15:20:00 +0100

    Exercice 2

--- array/app.js
+++ array/app.js
@@ -2,3 +2,4 @@ var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 var array = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"];
 
 console.log(array)
+console.log(array [2])
